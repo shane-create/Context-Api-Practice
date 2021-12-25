91cfdf933e02e457971a4f68b76138b0ab607d3f
xhane-create <shai0136@g2650.dk>
2021-12-25 17:52:22 +0100

    Tried creating my first context, and i also used .Consumer for the first time. There are 2 ways to get data from a context, and i am currently using the 2 method that is much more simple and uses the react hook useContext, but I have added the second method as a comment in case I will need it in the future!

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -1,13 +1,13 @@
-import React from 'react';
-import { connect } from 'react-redux';
+import React, {useContext} from 'react';
 
-import CollectionItem from '../../components/collection-item/collection-item.component';
-
-import { selectCollection } from '../../redux/shop/shop.selectors';
+import CollectionItem from '../../components/collection-item/collection-item.component'
+import CollectionsContext from '../../contexts/collections/collections.context';
 
 import './collection.styles.scss';
 
-const CollectionPage = ({ collection }) => {
+const CollectionPage = ({ match }) => {
+  const collections = useContext(CollectionsContext);
+  const collection = collections[match.params.collectionId];
   const { title, items } = collection;
   return (
     <div className='collection-page'>
@@ -21,8 +21,31 @@ const CollectionPage = ({ collection }) => {
   );
 };
 
-const mapStateToProps = (state, ownProps) => ({
-  collection: selectCollection(ownProps.match.params.collectionId)(state)
-});
+/*ONE WAY OF USING THE CONTEXT. TRY DELETING THE ABOVE AND REPLACING IT WITH THE STUFF UNDERNEATH AND 
+HOVER OVER THINGS YOU DON'T UNDERSTAND. IT WILL MAKE SENSE!*/
+/* 
+const CollectionPage = ({match}) => {
+  return(
+    <CollectionsContext.Consumer>
+      {collections => {
+          const collection = collections[match.params.collectionId];
+          const {title, items} = collection;
+          return(
+            <div className='collection-page'>
+              <h2 className='title'>{title}</h2>
+              <div className='items'>
+                {items.map(item => (
+                  <CollectionItem key={item.id} item={item} />
+                ))}
+              </div>
+            </div>
+          )
+        }
+      }
+    </CollectionsContext.Consumer>
+  )
+}
 
-export default connect(mapStateToProps)(CollectionPage);
+*/
+
+export default CollectionPage;
