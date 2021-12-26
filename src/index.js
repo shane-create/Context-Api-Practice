439880adfad1214251cbaf8913d09fcc880ee98a
xhane-create <shai0136@g2650.dk>
2021-12-26 22:12:14 +0100

    Sorry, just had to remove the last bit of reduxgit add .!

--- src/index.js
+++ src/index.js
@@ -1,25 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react';
 
 import CartProvider from './providers/cart/cart.provider';
-
-import { store, persistor } from './redux/store';
 
 import './index.css';
 import App from './App';
 
 ReactDOM.render(
-  <Provider store={store}>
     <CartProvider>
       <BrowserRouter>
-        <PersistGate persistor={persistor}>
           <App />
-        </PersistGate>
       </BrowserRouter>
-    </CartProvider>
-  </Provider>,
+    </CartProvider>,
   document.getElementById('root')
 );
